93e1e9ccaac14e59868ee46795f34270cf67e9f5
Anderson Piotto <piottok10@gmail.com>
2021-10-11 05:12:30 -0300

    adicionado spring security, permitindo todos os endpoints

---
+++ src/main/java/br/com/anderson/southsystem/desafiobackvotos/security/SecurityConfigurations.java
@@ -0,0 +1,33 @@
+package br.com.anderson.southsystem.desafiobackvotos.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+        .disable().authorizeRequests()
+		.antMatchers("/**").permitAll()
+		.antMatchers("/southsystem-desafio-voto/**").permitAll();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+        .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+	}
+
+}
