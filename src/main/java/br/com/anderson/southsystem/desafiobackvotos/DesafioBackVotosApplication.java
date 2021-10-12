b5413e5b55c8d9701db9781448433bd962f895e2
Anderson Piotto <piottok10@gmail.com>
2021-10-12 02:40:42 -0300

    ajuste de timeZone

--- src/main/java/br/com/anderson/southsystem/desafiobackvotos/DesafioBackVotosApplication.java
+++ src/main/java/br/com/anderson/southsystem/desafiobackvotos/DesafioBackVotosApplication.java
@@ -1,14 +1,29 @@
 package br.com.anderson.southsystem.desafiobackvotos;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@EnableKafka
 @SpringBootApplication
 public class DesafioBackVotosApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(DesafioBackVotosApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioBackVotosApplication.class, args);
 	}
+	
+	@PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+        log.info("TimeZone: " + new Date());
+    }
 
 }
